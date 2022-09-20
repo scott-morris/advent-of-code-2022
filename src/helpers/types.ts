5d7679fe143b567168fc0cc311d1860b5c19877e
Scott Morris <scott.r.morris@gmail.com>
2022-09-20 19:47:38 -0400

    Shared types

---
+++ src/helpers/types.ts
@@ -0,0 +1,12 @@
+export type Key = string | number;
+
+export interface MapCallbackFunction {
+    (value: any, key: Key, arr: Array<any>): void
+}
+
+export interface Result {
+    result: any;
+    duration: number;
+}
+  
+export type PotentialResult = Result | undefined;
